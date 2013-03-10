5d2e921fa356a27c39f300f1b51202b4659b7908
takanopontaro <takanopontaro@mail>
2013-03-10 20:09:39 +0900

    update

--- takanopontaro/label.js
+++ takanopontaro/label.js
@@ -3,20 +3,22 @@ imjs.plugin.label = {
 };
 
 imjs.addCommand(function () {
-	var c = imjs.plugin.label, els = imjs.getElements('.' + (imjs.prefix + '-label')), list = {}, css = imjs.getBrowserFromUa().ie6 ? ['height', 'auto'] : ['minHeight', 0];
+	var browser = imjs.getBrowserFromUa();
+	browser = {ie7:true};
+	if (!browser.ie6 && !browser.ie7 && !browser.ie8) return;
+	var c = imjs.plugin.label, els = imjs.getElements('.' + (imjs.prefix + '-label'));
 	imjs.forEach(els, function (el, i) {
-		var group = imjs.getAttribute(el, 'data-group') || '_';
-		(list[group] = (list[group] || [])).push(el);
+		var target = el;
+		if (!browser.ie6) {
+			var img = imjs.getElements('img', el);
+			if (img.length == 0) return;
+			target = img[0];
+		}
+		var inputs = imjs.getElements('input', el);
+		if (inputs.length == 0 || !/radio|checkbox/.test(inputs[0].type)) return;
+		imjs.on(target, 'click', function (e) {
+			if (e.srcElement == inputs[0]) return;
+			inputs[0].checked = !inputs[0].checked;
+		});
 	});
-	for (var group in list) {
-		var max = 0;
-		imjs.forEach(list[group], function (el, i) {
-			el.style[css[0]] = css[1];
-			var style = el.currentStyle || document.defaultView.getComputedStyle(el, '');
-			max = Math.max(max, Math.ceil(style.height.replace(/[^\.\d]/g, '')));
-		});
-		imjs.forEach(list[group], function (el, i) {
-			el.style[css[0]] = max + 'px';
-		});
-	}
 });
